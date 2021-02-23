f0325bcef388e0215fcb62cc4ea1e7eb720aabcf
Julia <zakr.jv@gmail.com>
2021-02-24 01:19:35 +0800

    Удалено создание лишнего объекта

--- js/main.js
+++ js/main.js
@@ -5,12 +5,10 @@ import {createAd} from './similar-ads.js'
 const ADS_CONTAINER = document.querySelector('#map-canvas');
 const TEMPLATE_AD = document.querySelector('#card').content.querySelector('.popup');
 
-// Создание объявления
-const similarAd = createObject();
-const card = createAd(similarAd, TEMPLATE_AD);
-ADS_CONTAINER.appendChild(card);
-
 // Создание массива из 10 объектов
 const similarObjects = new Array(10).fill(null).map(() => createObject());
-similarObjects;
+
+// Создание объявления
+const card = createAd(similarObjects[0], TEMPLATE_AD);
+ADS_CONTAINER.appendChild(card);
 
